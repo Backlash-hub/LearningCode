c08566e889370b123a907f62a81949bdcab42ca5
William Pevytoe <williampevytoe@gmail.com>
2023-02-26 21:03:23 -0500

    good to go

--- Objects.py
+++ Objects.py
@@ -1,12 +1,36 @@
-class PartyAnimal:
-    x = 0
+# class PartyAnimal:
+#     x = 0
     
-    def party(self):
-        self.x=self.x+1
-        print('So far', self.x)
+#     def party(self):
+#         self.x=self.x+1
+#         print('So far', self.x)
 
-an = PartyAnimal()
+# an = PartyAnimal()
     
-an.party()
-an.party()
-an.party()
+# an.party()
+# an.party()
+# an.party()
+
+#################
+
+# class PartyAnimal:
+#     x = 0
+#     name = ""
+#     def _init_(self, z):
+#         self.name = z
+#         print(self.name,"constructecd")
+    
+#     def party(self):
+#         self.x = self.x + 1
+#         print(self.name,"party count", self.x)
+
+# s = PartyAnimal("Sally")
+# s.party()
+
+# j = PartyAnimal("Jim")
+# j.party()
+# s.party()
+
+
+#########################
+
